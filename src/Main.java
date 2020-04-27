60c787edc13a864297ff9a83610051334d2f713a
立花冠 <tachibanakan@tachibanakannoMacBook-Pro.local>
2020-04-27 10:06:35 +0900

    progress

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 0x344;
+        int b = 111_333;
+        double c = 0.23211e1;
+        System.out.println('a');
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("It's time to eat breakfast\n Ok, I'm coming!");
+        System.out.println(c);
+
+
+
+        System.out.println(Arrays.deepToString(list));
+
+        var value = 10d;
+        System.out.println("ようこそ、\nJavaの世界へ");
+        String str = null;
+        int[][] data = new int[5][4];
+        var list = new int[][]{{2,3,5},{1,2},{10,11,12,13}};
+    }
+}
