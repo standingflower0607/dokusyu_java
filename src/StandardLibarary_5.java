80ca49a70d3c87040bc10c6e7c9d6f44aec4897e
立花冠 <tachibanaa1211@gmail.com>
2020-04-28 16:31:50 +0900

    progress

--- src/StandardLibarary_5.java
+++ src/StandardLibarary_5.java
@@ -1,5 +1,9 @@
+import java.util.regex.Pattern;
+import java.util.*;
+import java.time.*;
+
 public class StandardLibarary_5 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // str -> num  parseXxxx
         System.out.println(Integer.parseInt("333"));
         System.out.println(Integer.parseInt("FF", 16));
@@ -40,7 +44,7 @@ public class StandardLibarary_5 {
 
         System.out.println(str.contains("にわ"));
         System.out.println(str.startsWith("にわ"));
-        System.out.println(str.startsWith("にわ",8));
+        System.out.println(str.startsWith("にわ", 8));
         System.out.println(str.endsWith("いない"));
 
         // substring charAt
@@ -53,7 +57,47 @@ public class StandardLibarary_5 {
         var result1 = str1.split(",");
         System.out.println(String.join("&", result1));
 
-        
+        // regular expression
+        var tel = new String[]{"080-0000-0000", "084-000-0000", "184-0000"};
+        var rx = "\\d{2,4}-\\d{2,4}-\\d{4}";
+        for (var t : tel) {
+            System.out.println(Pattern.matches(rx, t) ? t : "unmatch");
+        }
+
+        var str2 = "会社の電話番号は0123-99-0000です。自宅は000-123-4567でございます。";
+        var ptn = Pattern.compile("(\\d{2,4})-(\\d{2,4})-(\\d{4})");
+        var match = ptn.matcher(str2);
+        // matcherを使うと一回でコンパイルが済むので、ループさせるときに便利
+        while (match.find()) {
+            System.out.println("開始位置:" + match.start());
+            System.out.println("終了位置:" + match.end());
+            System.out.println("マッチング文字列:" + match.group());
+            System.out.println("市外局番:" + match.group(1));
+            System.out.println("市内局番:" + match.group(2));
+            System.out.println("加入者番号:" + match.group(3));
+            System.out.println("---------------");
+        }
+
+        var str3 = "住所は160-0000 新宿区南町0-0-0です。\n あなたの住所は210-9999 川崎市北町1-1-1ですね";
+        var ptn3 = Pattern.compile("(\\d{3}-\\d{4})");
+        var match3 = ptn3.matcher(str3);
+        while (match3.find()) {
+            System.out.println(match3.group());
+        }
+
+        System.out.println(LocalDateTime.now());
+        System.out.println(OffsetDateTime.now());
+        System.out.println(ZonedDateTime.now());
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+
+        var dt1 = LocalDateTime.of(2019, 12, 31,10,20,30);
+        var dt2 = LocalDateTime.of(2019, 12,31,10,20,35);
+        System.out.println(dt1.equals(dt2));
+        System.out.println(dt1.isAfter(dt2));
+        System.out.println(dt1.isBefore(dt2));
+
+
 
     }
 }
